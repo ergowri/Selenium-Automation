7a442fc14f14efd710d619548eeb7fa179ad77ea
Gowri S <Gowri S@DESKTOP-AF33508.Dlink>
2023-03-15 18:02:19 +0530

    script to print window handle ID of current browser window

---
+++ Selenium-Automation/src/test/java/browser_actions_browser_window/Print_Window_Handle_ID_Current_Browser.java
@@ -0,0 +1,23 @@
+package browser_actions_browser_window;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//script to print window handle ID of current browser window
+public class Print_Window_Handle_ID_Current_Browser {
+	static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+
+		driver = new FirefoxDriver();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(2000);
+
+		// Return window handle ID of current browser window
+		System.out.println("Window handle ID of current browser window is: " + driver.getWindowHandle());
+
+		driver.quit();
+	}
+
+}
