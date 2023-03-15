6ce07b605d531a3fe70ac440eb1d6d80e96711ba
Gowri S <Gowri S@DESKTOP-AF33508.Dlink>
2023-03-15 16:45:10 +0530

    script to open google.com and verify that title is Google

---
+++ Selenium-Automation/src/test/java/browser_actions/Launch_WebPage_And_Verify_Title.java
@@ -0,0 +1,33 @@
+package browser_actions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+/**
+ * @see script to open google.com and verify that title is Google
+ *
+ */
+public class Launch_WebPage_And_Verify_Title {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+
+		// Launch google.com webpage
+		driver.get("https://www.google.com/");
+
+		// verify that title is Google
+		String expected_title = "Google";
+		String actual_title = driver.getTitle();
+		if (actual_title.equals(expected_title)) {
+			System.out.println("Pass: Title is Google");
+		} else {
+			System.out.println("Fail: Title is not Google");
+		}
+		// System.out.println(driver.getPageSource());
+
+		driver.quit();
+	}
+
+}
