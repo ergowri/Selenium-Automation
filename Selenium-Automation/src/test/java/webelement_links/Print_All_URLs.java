fdd35cefb650e7b5b1f99c47a91f8818bee9f800
Gowri S <Gowri S@DESKTOP-AF33508.Dlink>
2023-03-20 17:16:31 +0530

    script to print all the URLs present on the webpage

---
+++ Selenium-Automation/src/test/java/webelement_links/Print_All_URLs.java
@@ -0,0 +1,29 @@
+package webelement_links;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+//Script to print all the URL present on the page
+public class Print_All_URLs {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		driver = new EdgeDriver();
+		driver.get("https://www.google.co.in/");
+		List<WebElement> all_Links = driver.findElements(By.tagName("a"));
+
+		int number_of_links = all_Links.size();
+		System.out.println("Total number of links present on webpage: " + number_of_links);
+
+		for (WebElement urls : all_Links) {
+			System.out.println("URL present on the webpage" + urls.getAttribute("href"));
+
+		}
+		driver.quit();
+	}
+
+}
