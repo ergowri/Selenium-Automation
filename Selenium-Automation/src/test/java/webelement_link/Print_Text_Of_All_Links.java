84dcd476a9a55b43f43745b1343a28779ccc8f20
Gowri S <Gowri S@DESKTOP-AF33508.Dlink>
2023-03-24 15:18:14 +0530

    script to print text of links present on the page

---
+++ Selenium-Automation/src/test/java/webelement_link/Print_Text_Of_All_Links.java
@@ -0,0 +1,28 @@
+package webelement_link;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+//Write a script to print text of links present on the page?
+public class Print_Text_Of_All_Links {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		driver = new EdgeDriver();
+		driver.get("https://www.google.co.in/");
+		List<WebElement> all_Links = driver.findElements(By.tagName("a"));
+
+		int number_of_links = all_Links.size();
+		System.out.println("Total number of links present on webpage: " + number_of_links);
+
+		for (WebElement text_of_all_links : all_Links) {
+			System.out.println("Text of links present on a webpage: " + text_of_all_links.getText());
+		}
+		driver.quit();
+	}
+
+}
